321930fb8162e79ddc2ae95727147fe82305afc1
Russell Warwick <russellwarwick1998@gmail.com>
2022-04-08 00:32:49 +0100

    Refactors filter buttons into CategoriesView

--- TechChallenge/View/Categories View/CategoriesView.swift
+++ TechChallenge/View/Categories View/CategoriesView.swift
@@ -13,6 +13,24 @@ struct CategoryModel: Identifiable {
     let color: Color
 }
 
-
-
-
+struct CategoriesView: View {
+    
+    var updateFilter: (String?) -> Void
+    let categories: [CategoryModel] = [CategoryModel(category: "all", color: .black)] +
+    TransactionModel.Category.allCases.map({ CategoryModel(category: $0.rawValue, color: $0.color) })
+    
+    var body: some View {
+        ZStack {
+            Color.accentColor.opacity(0.8)
+            ScrollView(.horizontal) {
+                HStack(spacing: 5) {
+                    ForEach(categories, id: \.id) { item in
+                        CategoryView(category: item) {
+                            self.updateFilter((item.category == "all") ? nil : item.category)
+                        }
+                    }
+                }.padding()
+            }
+        }.frame(height: 50)
+    }
+}
--- TechChallenge/View/TransactionList/TransactionListView.swift
+++ TechChallenge/View/TransactionList/TransactionListView.swift
@@ -10,30 +10,18 @@ import SwiftUI
 struct TransactionListView: View {
     @State var filter: String?
     
-    private static var allCategory = CategoryModel(category: "all", color: .black)
-     
     let transactions: [TransactionModel] = ModelData.sampleTransactions
-    let categories: [CategoryModel] =  [TransactionListView.allCategory] +
-    TransactionModel.Category.allCases.map({ CategoryModel(category: $0.rawValue, color: $0.color) })
     
     var body: some View {
         VStack {
-            ZStack {
-                Color.accentColor.opacity(0.8)
-                ScrollView(.horizontal) {
-                    HStack(spacing: 5) {
-                        ForEach(categories, id: \.id) { item in
-                            CategoryView(category: item) {
-                                self.filter = item.category
-                            }
-                        }
-                    }.padding()
-                }
-            }.frame(height: 50)
+            
+            CategoriesView { filter in
+                self.filter = filter
+            }
             
             List {
                 ForEach(transactions.filter({
-                    guard let filter = $filter.wrappedValue, filter != TransactionListView.allCategory.category else { return true }
+                    guard let filter = $filter.wrappedValue else { return true }
                     return $0.category.rawValue == filter
                 })) { transaction in
                     TransactionView(transaction: transaction)
@@ -43,6 +31,8 @@ struct TransactionListView: View {
             .listStyle(PlainListStyle())
             .navigationBarTitleDisplayMode(.inline)
             .navigationTitle("Transactions")
+            
+      
         }
     }
 }
